a95e5449fb6230c979277cf60ede8683dd679132
Sidney Roberto <sidneyrpsilva@gmail.com>
2022-04-25 16:18:56 -0300

    ✨ feat: Create auth route and add in index.js

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const authRoutes = require("./routes/auth");
 
 dotenv.config();
+app.use(express.json());
 
 mongoose
   .connect(process.env.MONGO_URL, {
@@ -12,6 +14,8 @@ mongoose
   })
   .then(() => console.log("DB Connection Successful!"))
   .catch((err) => console.log(err));
+
+app.use("/api/auth", authRoutes);
 
 app.listen(8800, () => {
   console.log("Backend server is running on port 8800");
