7445b5d23088606c3a5b0f76af20ec29024cb3a0
Sidney Roberto <sidneyrpsilva@gmail.com>
2022-04-27 01:22:37 -0300

    ✨ feat: add create list router

---
+++ routes/lists.js
@@ -0,0 +1,25 @@
+const router = require("express").Router();
+const List = require("../models/List");
+const verify = require("../verifyToken");
+
+//CREATE
+router.post("/", verify, async (req, res) => {
+  if (req.user.isAdmin) {
+    const newList = new List(req.body);
+
+    try {
+      const savedList = await newList.save();
+      res.status(201).json(savedList);
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  } else {
+    res.status(403).json("You are not allowed!");
+  }
+});
+
+//DELETE
+
+//GET
+
+module.exports = router;
