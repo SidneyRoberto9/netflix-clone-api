c94926e96e17a6f888efa6d7709d887a23026907
Sidney Roberto <sidneyrpsilva@gmail.com>
2022-04-27 00:28:43 -0300

    ✨ feat: add update movie router

--- routes/movies.js
+++ routes/movies.js
@@ -18,4 +18,24 @@ router.post("/", verify, async (req, res) => {
   }
 });
 
+//UPDATE
+router.put("/:id", verify, async (req, res) => {
+  if (req.user.isAdmin) {
+    try {
+      const updatedMovie = await Movie.findByIdAndUpdate(
+        req.params.id,
+        {
+          $set: req.body,
+        },
+        { new: true }
+      );
+      res.status(200).json(updatedMovie);
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  } else {
+    res.status(403).json("You are not allowed!");
+  }
+});
+
 module.exports = router;
