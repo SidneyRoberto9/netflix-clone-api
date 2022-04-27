414f1940ccf5e7e2615ce5e781cdb29f71cf954e
Sidney Roberto <sidneyrpsilva@gmail.com>
2022-04-27 00:28:23 -0300

    ✨ feat: add create movie router

---
+++ routes/movies.js
@@ -0,0 +1,21 @@
+const router = require("express").Router();
+const Movie = require("../models/Movie");
+const verify = require("../verifyToken");
+
+//CREATE
+router.post("/", verify, async (req, res) => {
+  if (req.user.isAdmin) {
+    const newMovie = new Movie(req.body);
+
+    try {
+      const savedMovie = await newMovie.save();
+      res.status(201).json(savedMovie);
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  } else {
+    res.status(403).json("You are not allowed!");
+  }
+});
+
+module.exports = router;
